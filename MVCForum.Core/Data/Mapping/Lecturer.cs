1a4e5fb7901de6384463c68b647b0bf24dcfbce8
Unknown <170418S@mymail.nyp.edu.sg>
2019-01-23 11:18:07 +0800

    Fixed Weird Not In Solution Bug

---
+++ MVCForum.Core/Data/Mapping/Lecturer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcForum.Core.Models.Entities
+{
+    public partial class Lecturer
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        public int LecturerId { get; set; }
+        public string LectName { get; set; }
+        public string LectEmail { get; set; }
+        public int LectNo { get; set; }
+        public string ModuleGrp { get; set; }
+        public string ModuleId { get; set; }
+
+    }
+}
